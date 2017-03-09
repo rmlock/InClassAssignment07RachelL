af5ce779bd471740f1c68eaa7836b77de5a7525d
Rachel Lockerman <rl2856@tc.columbia.edu>
2017-03-08 20:24:52 -0500

    Wrote to database

--- app/src/main/java/com/example/android/secret/MainActivity.java
+++ app/src/main/java/com/example/android/secret/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.android.secret;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void saveSecret(View view) {
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("secret");
+
+        myRef.setValue("Hello, World!");
+        myRef.setValue("message");
+        EditText editText = (EditText) findViewById(R.id.secretEntered);
+        String secret = editText.getText().toString();
+        myRef.setValue(secret);
+
+    }
+
+    public void revealSecret(View view) {
+
+
     }
 }
