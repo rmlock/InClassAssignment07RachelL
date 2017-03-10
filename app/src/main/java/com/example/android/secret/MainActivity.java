a5e63f52e9e48d2b0c1374feaa683fb0015d8785
Rachel Lockerman <rl2856@tc.columbia.edu>
2017-03-09 19:41:07 -0500

    Set up child firebase.

--- app/src/main/java/com/example/android/secret/MainActivity.java
+++ app/src/main/java/com/example/android/secret/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.android.secret;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -19,17 +21,25 @@ public class MainActivity extends AppCompatActivity {
     public void saveSecret(View view) {
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("secret");
+        EditText named = (EditText) findViewById(R.id.name);
+        String name = named.getText().toString();
+
+        DatabaseReference myRef = database.getReference(name).child("secret");
 
         myRef.setValue("Hello, World!");
         myRef.setValue("message");
         EditText editText = (EditText) findViewById(R.id.secretEntered);
         String secret = editText.getText().toString();
+
+        Toast.makeText(this, "Secret Saved!", Toast.LENGTH_SHORT).show();
         myRef.setValue(secret);
 
     }
 
     public void revealSecret(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+
 
 
     }
